5c59b445b5f9541d9bb2ca637b5feea44e785bd5
Fancisco Cabral <franciscocabralfilho@gmail.com>
2016-04-21 18:39:13 -0300

    Criação da base do servidor. Falta implementar a comunicação com o banco ou com o serviço de banco de dados, e a resposta que será dada ao cliente.

--- Server/src/main/java/com/frannciscocabral/ufs/rabbitmq/Receiver.java
+++ Server/src/main/java/com/frannciscocabral/ufs/rabbitmq/Receiver.java
@@ -5,37 +5,105 @@
  */
 package com.frannciscocabral.ufs.rabbitmq;
 
+import com.frannciscocabral.ufs.utils.Message;
+import com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
+import java.io.UnsupportedEncodingException;
 
 /**
  *
  * @author Francisco Cabral
  */
-public class Receiver {
-    public static void main(String[] argv) throws java.io.IOException, java.lang.InterruptedException, TimeoutException {
-
-        Connection connection = rmqConfig.init();
-        Channel channel = connection.createChannel();
-
-        String queue = "minhaFila";
-        channel.queueDeclare(queue, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-
-        Consumer consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
-            }
-        };
-        channel.basicConsume(queue, true, consumer);
+public class Receiver extends Thread {
+    
+    public static void addUser(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String[] data = g.fromJson(m.content, String[].class);
+        String name = data[0];
+        String email = data[1];
+        String password = data[2];
+        System.out.println(data[0]+" "+data[1]+" "+data[2]);
+        throw new Exception("Not implemented yet");
+    }
+    
+    public static void getUser(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String userId = m.content;
+        System.out.println(m.content);
+        throw new Exception("Not implemented yet");
+    }
+    public static void verifyUser(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String[] data = g.fromJson(m.content, String[].class);
+        String email = data[0];
+        String password = data[1];
+        System.out.println(data[0]+" "+data[1]);
+        throw new Exception("Not implemented yet");
+    }
+    
+    public static void getMessages(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String[] data = g.fromJson(m.content, String[].class);
+        String userId = data[0];
+        String contactId = data[1];
+        System.out.println(data[0]+" "+data[1]);
+        throw new Exception("Not implemented yet");
+    }
+    
+    public static void getContacts(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String userId = m.content;
+        System.out.println(m.content);
+        throw new Exception("Not implemented yet");
+    }
+    
+    public static void addContact(byte[] body) throws UnsupportedEncodingException, Exception{
+        Gson g = new Gson();
+        Message m = g.fromJson(new String(body, "UTF-8"), Message.class); 
+        String[] data = g.fromJson(m.content, String[].class);
+        String userId = data[0];
+        String contactId = data[1];
+        System.out.println(data[0]+" "+data[1]);
+        throw new Exception("Not implemented yet");
+    }
+    
+    
+    @Override
+    public void run() {
+        try {
+            Consumer consumer = new DefaultConsumer(Rmq.init().createChannel()) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    
+                    Gson g = new Gson();
+                    Message m = g.fromJson(new String(body, "UTF-8"), Message.class);
+                    System.out.println(" [x] Received: '" + new String(body) + "'");
+                    try{
+                        switch(m.type){
+                            case "ADDUSER": Receiver.addUser(body); break;
+                            case "GETUSER": Receiver.getUser(body); break;
+                            case "VERIFYUSER": Receiver.verifyUser(body); break;
+                            case "GETMESSAGES": Receiver.getMessages(body); break;
+                            case "GETCONTACTS": Receiver.getContacts(body); break;
+                            case "ADDCONTACT": Receiver.addContact(body); break;
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            };
+            Rmq.receiveMessage(consumer);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
---
+++ Server/src/main/java/com/frannciscocabral/ufs/utils/Message.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.frannciscocabral.ufs.utils;
+
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author Fancisco
+ */
+public class Message {
+
+    public String content;
+    public String type;
+
+    public Message() {}
+    
+    public Message(String content) {
+        this.content = content;
+        this.type = "none";
+    }
+    public Message(String type, String content) {
+        this.content = content;
+        this.type = type;
+    }
+    
+    public static Message addUser(String name, String email, String password){
+        String[] s = {name, email, password};
+        Gson g = new Gson();
+        return new Message("ADDUSER", g.toJson(s));
+    }
+    
+    public static Message getUser(int userId){
+        return new Message("GETUSER", Integer.toString(userId));
+    }
+    
+    public static Message verifyUser(String email, String password){
+        String[] s = {email, password};
+        Gson g = new Gson();
+        return new Message("VERIFYUSER", g.toJson(s));
+    }
+    
+    public static Message getMessages(int userId, int contactId){
+        String[] s = {Integer.toString(userId), Integer.toString(contactId)};
+        Gson g = new Gson();
+        return new Message("GETMESSAGES", g.toJson(s));
+    }
+    
+    public static Message getContacts(int userId){
+        return new Message("GETCONTACTS", Integer.toString(userId));
+    }
+    
+    public static Message addContact(int userId, int contactId){
+        String[] s = {Integer.toString(userId), Integer.toString(contactId)};
+        Gson g = new Gson();
+        return new Message("ADDCONTACT", g.toJson(s));
+    }
+    
+    
+    public String toJson(){
+        Gson g = new Gson();
+        return g.toJson(this);
+    }
+}
